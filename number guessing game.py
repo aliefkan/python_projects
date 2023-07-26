292a729bc85d98fd32feeb885588132948842957
aliefkan <ali.efkan@hotmail.com.tr>
2023-07-26 13:52:53 +0300

    Commit

---
+++ number guessing game.py
@@ -0,0 +1,19 @@
+import random
+game = False
+play = input("Would you like to play a game?(Y/N)")
+if play.lower() == "y":
+    game = True
+else:
+    pass
+tries = 0
+while game:
+    tries += 1
+    the_random_number = random.randint(0, 100)
+    player_guess = int(input("Guess a number 0 to 100: "))
+    if int(player_guess) is True:
+        print("You should enter a number.")
+    elif int(player_guess) == the_random_number:
+        print(f"Congratulations! You did it in {tries} try!")
+        game = False
+    else:
+        print("Wrong guess! Try again.")
